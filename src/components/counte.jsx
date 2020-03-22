c5704d23e571f09fcda345edc7482f5c49160b97
Abhishek Aryan <send2abhishek@live.com>
2020-03-22 22:04:22 +0530

    Conditional rendering

--- src/components/counte.jsx
+++ src/components/counte.jsx
@@ -3,7 +3,8 @@ class Counter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0
+      count: 0,
+      tech: []
     };
   }
 
@@ -13,14 +14,29 @@ class Counter extends Component {
   };
   render() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count == 0 ? "warning" : "danger";
+    classes += this.state.count === 0 ? "warning" : "primary";
     return (
       <div>
         <span style={this.styles} className={classes}>
           {this.formatCounter()}
         </span>
         <button className="btn btn-danger btn-sm">Increment</button>
+        {this.state.tech.length === 0 && <p>There is no tech for this</p> && (
+          <p>last statement</p>
+        )}
+        {this.renderHelper()}
       </div>
+    );
+  }
+
+  renderHelper() {
+    if (this.state.tech.length === 0) return <p>There is no tech</p>;
+    return (
+      <ul>
+        {this.state.tech.map(data => (
+          <li key={data}>{data}</li>
+        ))}
+      </ul>
     );
   }
 
